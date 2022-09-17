1ec459f2a90459e2918099a781aa8dd76df6da66
20P31A04E2 <84621897+20P31A04E2@users.noreply.github.com>
2022-09-17 21:13:03 +0530

    import java.util.Scanner; class Mini {     public static void main(String args[])     {         int n,i,x[];         Scanner sc = new Scanner (System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int temp=x[0];         for(i=0;i<n;i++)         {            if(x[i]>temp)            {                temp=x[i];            }         }         System.out.println(temp);     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Mini
+{
+    public static void main(String args[])
+    {
+        int n,i,x[];
+        Scanner sc = new Scanner (System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int temp=x[0];
+        for(i=0;i<n;i++)
+        {
+           if(x[i]>temp)
+           {
+               temp=x[i];
+           }
+        }
+        System.out.println(temp);
+    }
+}
