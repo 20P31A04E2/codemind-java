b158225ae029964bb9d4b69bd3bf1407f6ad10b1
20P31A04E2 <84621897+20P31A04E2@users.noreply.github.com>
2022-10-01 11:31:58 +0530

    import java.util.Scanner; class ele {     public static void main(String args[])     {         int n,x[],i,flag=0;         Scanner sc = new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(x[i]%2!=0)             {                 if(i%2!=0)                 continue;                 else                 flag++;             }         }         if(flag >0)         System.out.println("False");         else         System.out.println("True");     } }

---
+++ Strictly_ODD.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class ele
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,flag=0;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2!=0)
+            {
+                if(i%2!=0)
+                continue;
+                else
+                flag++;
+            }
+        }
+        if(flag >0)
+        System.out.println("False");
+        else
+        System.out.println("True");
+    }
+}
