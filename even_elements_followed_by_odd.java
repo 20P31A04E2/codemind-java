bb5395cab5da41de1d086a4a791203b357a9024d
20P31A04E2 <84621897+20P31A04E2@users.noreply.github.com>
2022-10-07 14:37:18 +0530

    import java.util.Scanner; class Arr {     public static void main(String args[])     {         int n,x[],even[],odd[],i,j=0,k=0;         Scanner sc = new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         even=new int[n];         odd=new int[n];         for(i=0;i<n;i++)         {             if(x[i]%2==0)             {                 even[j]=x[i];                 j++;             }             else             {                 odd[k]=x[i];                 k++;             }         }         for(i=0;i<j;i++)         System.out.print(even[i]+" ");         for(i=0;i<k;i++)         System.out.print(odd[i]+" ");     } }

--- even_elements_followed_by_odd.java
+++ even_elements_followed_by_odd.java
@@ -1,25 +1,34 @@
 import java.util.Scanner;
-class ele
+class Arr
 {
     public static void main(String args[])
     {
-        int n,x[],i;
-        Scanner sc = new Scanner (System.in);
+        int n,x[],even[],odd[],i,j=0,k=0;
+        Scanner sc = new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
         for(i=0;i<n;i++)
         {
             x[i]=sc.nextInt();
         }
+        even=new int[n];
+        odd=new int[n];
         for(i=0;i<n;i++)
         {
-        if(x[i]%2==0)
-        System.out.print(x[i]+" ");
+            if(x[i]%2==0)
+            {
+                even[j]=x[i];
+                j++;
+            }
+            else
+            {
+                odd[k]=x[i];
+                k++;
+            }
         }
-        for(i=0;i<n;i++)
-        {
-        if(x[i]%2!=0)
-        System.out.print(x[i]+" ");
-        }
+        for(i=0;i<j;i++)
+        System.out.print(even[i]+" ");
+        for(i=0;i<k;i++)
+        System.out.print(odd[i]+" ");
     }
 }
