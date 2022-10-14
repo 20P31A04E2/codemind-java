a24780f97c3b8bc9f4b877a6894f7bdd2e189293
20P31A04E2 <84621897+20P31A04E2@users.noreply.github.com>
2022-10-14 22:08:54 +0530

    import java.util.Scanner; class Ele {     public static void main(String args[])     {         int n,x[],i,j,sum=0;         Scanner sc = new Scanner (System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(x[i]!=-999)             {                 for(j=0;j<n;j++)                 {                     if(i!=j && x[i]==x[j])                     {                         x[j]=-999;                     }                 }                 sum=sum+x[i];             }         }         System.out.println(sum);     } }

---
+++ unique_sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Ele
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,j,sum=0;
+        Scanner sc = new Scanner (System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]!=-999)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(i!=j && x[i]==x[j])
+                    {
+                        x[j]=-999;
+                    }
+                }
+                sum=sum+x[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
