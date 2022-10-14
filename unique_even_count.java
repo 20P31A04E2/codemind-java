e007e45cfff6fdb3ea55f267a27b921bff86005e
20P31A04E2 <84621897+20P31A04E2@users.noreply.github.com>
2022-10-14 22:27:20 +0530

    import java.util.Scanner; class Ele {     public static void main(String args[])     {         int n,x[],i,j,count=1,flag=0;         Scanner sc = new Scanner (System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(x[i]!=-999)             {                 for(j=0;j<n;j++)                 {                     if(i!=j && x[i]==x[j])                     {                         count++;                         x[j]=-999;                     }                 }                 if(x[i]%2==0)                 flag++;             }         }         System.out.println(flag);     } }

---
+++ unique_even_count.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Ele
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,j,count=1,flag=0;
+        Scanner sc = new Scanner (System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]!=-999)
+            {
+                for(j=0;j<n;j++)
+                {
+                    if(i!=j && x[i]==x[j])
+                    {
+                        count++;
+                        x[j]=-999;
+                    }
+                }
+                if(x[i]%2==0)
+                flag++;
+            }
+        }
+        System.out.println(flag);
+    }
+}
